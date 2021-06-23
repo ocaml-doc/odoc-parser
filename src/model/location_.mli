b4caf6858dcf0f14607971941f33f3d2153c72e1
Jon Ludlam <jon@recoil.org>
2021-06-23 22:26:05 +0100

    Remove non-odoc-parser stuff

--- src/model/location_.mli
+++
@@ -1,7 +0,0 @@
-include module type of struct
-  include Odoc_parser.Loc
-end
-
-val set_end_as_offset_from_start : int -> span -> span
-
-val in_string : string -> offset:int -> length:int -> span -> span
